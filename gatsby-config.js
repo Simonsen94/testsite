f7bea51dd8bbe704ed2337a6f876df8ae9020d5b
Simonsen94 <soerenkristiansimonsen@gmail.com>
2021-10-26 09:56:47 +0200

    Installing UA and Optimize on site

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,36 @@
 module.exports = {
   plugins: [
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-208583749-1", // Google Analytics / GA
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "OPT-PZ6DN94",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
+    'gatsby-theme-codebushi',
+    {
       resolve: `gatsby-theme-codebushi`,
       options: {
         tailwindConfig: `tailwind.config.js`
-      }
-    }
+    },  
+  },
   ]
-};
+}
