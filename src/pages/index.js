b645e5fa85ebfa0eb1b14e77dc312d12abbdcf63
Simonsen94 <soerenkristiansimonsen@gmail.com>
2021-12-29 10:52:44 +0100

    removing one css file + changing font size

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,10 +15,10 @@ const Index = () => (
     <section className="pt-20 md:pt-40">
       <div className="container mx-auto px-8 lg:flex">
         <div className="text-center lg:text-left lg:w-1/2">
-          <h2 className="text-4xl lg:text-5xl xl:text-6xl font-bold leading-none">
+          <h2 className="text-3xl lg:text-5xl xl:text-6xl font-bold leading-none">
             From data to actionable insights.
           </h2>
-          <p className="text-xl lg:text-2xl mt-6 font-light">
+          <p className="text-lg lg:text-2xl mt-6 font-light">
             SMEs face a unique set of challenges in becoming data-driven.
             The SME Analyst, solve these <em>and</em> rapidly grows your digital business.
           <br/><br/>
@@ -47,7 +47,7 @@ const Index = () => (
         </div>
       </div>
     </section>
-    <section id="features" className="py-20 lg:pb-40 lg:pt-48">
+    <section id="services" className="py-20 lg:pb-40 lg:pt-48">
       <div className="container mx-auto text-center">
         <h2 className="text-3xl lg:text-5xl font-semibold">My services include</h2>
         <div className="flex flex-col sm:flex-row sm:-mx-3 mt-12">
@@ -94,7 +94,7 @@ const Index = () => (
       </div>
     </section>
     <SplitSection
-      id="services"
+      id="features"
       primarySlot={
         <div className="lg:pr-32 xl:pr-48">
           <h3 className="text-3xl font-semibold leading-tight">Market Analysis</h3>
