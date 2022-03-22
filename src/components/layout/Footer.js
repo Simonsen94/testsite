a413d59d30d7a705cbc011fa25bed6dab521c63a
Simonsen94 <soerenkristiansimonsen@gmail.com>
2022-03-22 12:34:18 +0100

    footer change. remove personal link + shorten description

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,15 +5,14 @@ const Footer = () => (
     <div className="flex -mx-3">
       <div className="flex-1 px-3">
         <h2 className="text-lg font-semibold">About Me</h2>
-        <p className="mt-5">I'm a Copenhagen based Optimization and Analytics specialist with more
-        than 2 years of experience helping B2B and B2C companies. My educational background is within
-        eBusiness and I hold a MSc in Business Administration and eBusiness from CBS.</p>
+        <p className="mt-5">Copenhagen based Data Analyst with more
+        than 2 years of experience helping B2B and B2C companies scale digitally.</p>
       </div>
       <div className="flex-1 px-30">
         <h2 className="text-lg font-semibold">Social Media</h2>
         <ul className="mt-4 leading-loose">
           <li>
-            <a href="https://www.linkedin.com/in/s%C3%B8ren-kristian-simonsen/" 
+            <a href="https://www.linkedin.com" 
             target="_blank" rel="noopener noreferrer">LinkedIn</a>
           </li>
         </ul>
